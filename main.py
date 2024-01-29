949224800f97004ba6276921037a2da985678c85
oldmcdonnell <oldmcdonnell@gmail.com>
2024-01-29 14:46:03 -0500

    pulled tour data from page

--- main.py
+++ main.py
@@ -3,12 +3,20 @@ import selectorlib
 
 
 URL = "https://programmer100.pythonanywhere.com/tours/"
-
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 def scrape(url):
-    #scrape the page source from the UTL
-    reponse = requests.get(url)
-    source = reponse.text
+    """scrape the page source from the URL"""
+    response = requests.get(url, headers=HEADERS)
+    source = response.text
     return source
 
+def extract(source):
+    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+    value = extractor.extract(source)["tours"]
+    return value
+
 if __name__ == "__main__":
-    print(scrape(URL))
+    scraped = scrape(URL)
+    extracted = extract(scraped)
+    print(extracted)
